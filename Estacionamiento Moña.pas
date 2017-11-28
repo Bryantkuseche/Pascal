4a9f9e6c4572170625b83dbbaa2aac861a8a83da
Bryan Useche <bryan.useche.10@gmail.com>
2017-11-28 00:06:17 -0400

    Project of College 50% finished
    Add arrays, cases sentences, to do the 2nd option

--- Estacionamiento Moña.pas
+++ Estacionamiento Moña.pas
@@ -1,113 +1,149 @@
 PROGRAM Estacionamiento;
-var tipo, entrada, puesto, ticket, tipo_persona :INTEGER;
-var total:REAL;
-var fecha, salida, hora_entrada , hora_salida, rif:STRING;
-
+var factura, tipo_vehiculo, persona, id, i, entrada:INTEGER;
+var fecha_ent , fecha_sal, hora_ent , hora_sal:STRING;
+Puestos: array [1..100] of INTEGER;
+Vehiculos: array [1..100] of INTEGER;
+Fecha: array [1..100] of String;
+Horas: array [1..100] of String;
+Facturas: array [1..100] of INTEGER;
+tipo_persona: array [1..100] of INTEGER;
+Personas: array [1..100] of INTEGER;
 BEGIN
 	randomize;
+	for i:=1 to 100 do
+		BEGIN
+			Puestos[i]:=0;
+		END;
 	writeln('Parking lot system ver 1.0');
 	writeln('Seleccione tipo de operacion');
 	writeln('1 - Entrada de Vehiculos (Expedir ticket)');
 	writeln('2 - Salida de Vehiculos');
 	readln(entrada);
-	if entrada = 1 then
-	BEGIN
-		writeln('Indique que tipo de vehiculo es');
-		writeln('1 - Automovil particular');
-		writeln('2 - Transporte Publico');
-		writeln('3 - Transporte Pesado');
-		writeln('4 - Taxi');
-		readln(tipo);
-		ticket:=random(1000);
-		puesto:=random(100);
-		if tipo = 1 then
-		BEGIN
-			writeln('Introduzca la fecha de entrada');
-			readln(fecha);
-			writeln('Ticket Nº ' , ticket , ' creado exitosamente');
-			writeln('Puesto Nº ', puesto , ' y entra el dia ', fecha );
-
-		END;
-		else
-		if tipo = 2 then
-		BEGIN
-			writeln('Introduzca la fecha de entrada');
-			readln(fecha);
-			writeln('Ticket Nº ' , ticket , ' creado exitosamente');
-			writeln('Puesto Nº ', puesto , ' y entra el dia ', fecha );
-		END;
-		else
-		if tipo = 3 then
-		BEGIN
-			writeln('Introduzca la fecha de entrada');
-			readln(fecha);
-			writeln('Ticket Nº ' , ticket , ' creado exitosamente');
-			writeln('Puesto Nº ', puesto , ' y entra el dia ', fecha );
-		END;
-		else
-		if tipo = 4 then
-		BEGIN
-			writeln('Introduzca la fecha de entrada');
-			readln(fecha);
-			writeln('Introduzca la hora de entrada')
-			writeln('Ticket Nº ' , ticket , ' creado exitosamente');
-			writeln('Puesto Nº ', puesto , ' y entra el dia ', fecha );
-		END;
-	END;
-	else
-	if entrada = 2 then
-	BEGIN
-		writeln('Generador de pagos');
-		writeln('Introduzca el tipo de vehiculo:');
-		writeln('1 - Automovil particular');
-		writeln('2 - Transporte Publico');
-		writeln('3 - Transporte Pesado');
-		writeln('4 - Taxi');
-		readln(tipo);
-		if tipo = 1 then
-		BEGIN
-			writeln('Introduzca la fecha de entrada');
-			readln(fecha);
-			writeln('Introduzca la hora de entrada');
-			readln(hora_entrada);
-			writeln('Introduzca el tipo de persona el cual realizara el pago');
-			writeln('1 - Persona Natural');
-			writeln('2 - Persona Juridica');
-			readln(tipo_persona);
-			if tipo_persona = 1 then
-			BEGIN
-				writeln('Introduzca el RIF de la Persona');
-				readln(rif);
-				writeln('Introduzca la hora de salida');
-				readln(hora_salida);
-
+	CASE entrada OF
+		1: BEGIN
+			for i:=1 to 100 do
+				BEGIN
+					if Puestos[i] = 0 then
+						writeln('Bienvenido');
+						writeln('Introduzca el tipo de vehiculo');
+						writeln('1 - Automovil');
+						writeln('2 - Taxi');
+						writeln('3 - Bus');
+						writeln('4 - Camion');
+						readln(tipo_vehiculo);
+						CASE tipo_vehiculo OF
+							1: BEGIN
+								Puestos[i]:=1;
+								writeln('Introduzca la fecha de entrada');
+								readln(fecha_ent);
+								Fecha[i]:=fecha_ent;
+								writeln('Introduzca la hora de entrada');
+								readln(hora_ent);
+								Horas[i]:=hora_ent;
+								writeln('Introduzca si la persona es Juridica o Natural');
+								writeln('1 - Para Persona Juridica');
+								writeln('2 - Para Persona Natural');
+								readln(persona);
+								tipo_persona[i]:=persona;
+								writeln('Introduzca Cedula o RIF de la persona');
+								readln(id);
+								Personas[i]:=id;
+								Vehiculos[i]:=1;
+								Facturas[i]:=random(1000);
+								writeln('Bienvenido ' , Personas[i]);
+								writeln('Su puesto asignado es: ', Puesto[i]);
+								writeln('Su Nº de factura es: ', Facturas[i]);
+								writeln('Disfrute su estancia.');
+							END;
+							2: BEGIN
+								Puestos[i]:=1;
+								writeln('Introduzca la fecha de entrada');
+								readln(fecha_ent);
+								Fecha[i]:=fecha_ent;
+								writeln('Introduzca la hora de entrada');
+								readln(hora_ent);
+								Horas[i]:=hora_ent;
+								writeln('Introduzca si la persona es Juridica o Natural');
+								writeln('1 - Para Persona Juridica');
+								writeln('2 - Para Persona Natural');
+								readln(persona);
+								tipo_persona[i]:=persona;
+								writeln('Introduzca Cedula o RIF de la persona');
+								readln(id);
+								Personas[i]:=id;
+								Vehiculos[i]:=2;
+								Facturas[i]:=random(1000);
+								writeln('Bienvenido ' , Personas[i]);
+								writeln('Su puesto asignado es: ', Puesto[i]);
+								writeln('Su Nº de factura es: ', Facturas[i]);
+								writeln('Disfrute su estancia.');
+							END;
+							3: BEGIN
+								Puestos[i]:=1;
+								writeln('Introduzca la fecha de entrada');
+								readln(fecha_ent);
+								Fecha[i]:=fecha_ent;
+								writeln('Introduzca la hora de entrada');
+								readln(hora_ent);
+								Horas[i]:=hora_ent;
+								writeln('Introduzca si la persona es Juridica o Natural');
+								writeln('1 - Para Persona Juridica');
+								writeln('2 - Para Persona Natural');
+								readln(persona);
+								tipo_persona[i]:=persona;
+								writeln('Introduzca Cedula o RIF de la persona');
+								readln(id);
+								Personas[i]:=id;
+								Vehiculos[i]:=3;
+								Facturas[i]:=random(1000);
+								writeln('Bienvenido ' , Personas[i]);
+								writeln('Su puesto asignado es: ', Puesto[i]);
+								writeln('Su Nº de factura es: ', Facturas[i]);
+								writeln('Disfrute su estancia.');
+							END;
+							4: BEGIN
+								Puestos[i]:=1;
+								writeln('Introduzca la fecha de entrada');
+								readln(fecha_ent);
+								Fecha[i]:=fecha_ent;
+								writeln('Introduzca la hora de entrada');
+								readln(hora_ent);
+								Horas[i]:=hora_ent;
+								writeln('Introduzca si la persona es Juridica o Natural');
+								writeln('1 - Para Persona Juridica');
+								writeln('2 - Para Persona Natural');
+								readln(persona);
+								tipo_persona[i]:=persona;
+								writeln('Introduzca Cedula o RIF de la persona');
+								readln(id);
+								Personas[i]:=id;
+								Vehiculos[i]:=4;
+								Facturas[i]:=random(1000);
+								writeln('Bienvenido ' , Personas[i]);
+								writeln('Su puesto asignado es: ', Puesto[i]);
+								writeln('Su Nº de factura es: ', Facturas[i]);
+								writeln('Disfrute su estancia.');
+							END;
+							Else BEGIN
+								writeln('Opcion invalida');
+							END;
+						END;
+					else BEGIN
+					writeln('Puesto ' , i , ' lleno');
+					writeln('buscando uno vacio');
+					END;
 			END;
-
-
+		2: BEGIN
+			writeln('Indique el numero de factura');
+			readln(factura);
+			for i:1 to 100 do
+				BEGIN
+					if factura = i then
+						writeln('Introduzca fecha de salida');
+						readln(fecha_sal);
+						writeln('Introduzca hora de salida');
+						readln(hora_sal);
+				END;
+			END;
 		END;
-		else
-		if tipo = 2 then
-		BEGIN
-			writeln('Introduzca la fecha de entrada');
-			readln(fecha);
-			writeln('Ticket Nº ' , ticket , ' creado exitosamente');
-			writeln('Puesto Nº ', puesto , ' y entra el dia ', fecha );
-		END;
-		else
-		if tipo = 3 then
-		BEGIN
-			writeln('Introduzca la fecha de entrada');
-			readln(fecha);
-			writeln('Ticket Nº ' , ticket , ' creado exitosamente');
-			writeln('Puesto Nº ', puesto , ' y entra el dia ', fecha );
-		END;
-		else
-		if tipo = 4 then
-		BEGIN
-			writeln('Introduzca la fecha de entrada');
-			readln(fecha);
-			writeln('Ticket Nº ' , ticket , ' creado exitosamente');
-			writeln('Puesto Nº ', puesto , ' y entra el dia ', fecha );
-		END;
-	END;
 END.
